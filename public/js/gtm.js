05dcc248325e922e09ef63722bfbaa8e9f710917
Stephen Bunting <stebunting@gmail.com>
2020-08-07 11:44:28 +0200

    Added dataLayer to order placed page

--- public/js/gtm.js
+++ public/js/gtm.js
@@ -1,15 +1,13 @@
-dataLayer = [];
-
-(function(w,d,s,l,i){
-  w[l]=w[l]||[];
-  w[l].push({
-    'gtm.start':new Date().getTime(),
-    event:'gtm.js'
+(function gtm(win, doc, script, layer, id) {
+  win[layer] = win[layer] || [];
+  win[layer].push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js'
   });
-  var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),
-  dl=l!='dataLayer'?'&l='+l:'';
-  j.async=true;
-  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-  f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N8LRXL9');
+  const f = doc.getElementsByTagName(script)[0];
+  const j = doc.createElement(script);
+  const dl = layer !== 'dataLayer' ? `&l=${layer}` : '';
+  j.async = true;
+  j.src = `https://www.googletagmanager.com/gtm.js?id=${id}${dl}`;
+  f.parentNode.insertBefore(j, f);
+}(window, document, 'script', 'dataLayer', 'GTM-N8LRXL9'));
--- src/controllers/whiskController.js
+++ src/controllers/whiskController.js
@@ -11,9 +11,57 @@ const managementBaseUrl = process.env.MANAGEMENT_BASE_URL;
 const googleApiKey = process.env.GOOGLE_API_KEY;
 
 function whiskController() {
+  // Function to parse post data and call backend for statement
+  async function getStatement(postData) {
+    // Get Products Ordered
+    let basket = [];
+    const orders = Object.entries(postData).filter((x) => x[0].startsWith('quantity-'));
+    for (let i = 0; i < orders.length; i += 1) {
+      const [, id] = orders[i][0].split('-');
+      const quantity = parseInt(orders[i][1], 10);
+      if (quantity > 0) {
+        basket.push({ id, quantity });
+      }
+    }
+    basket = JSON.stringify(basket);
+
+    // Get Delivery Information
+    let zone2Deliveries;
+    let zone3Deliveries;
+    if (postData['delivery-type'] === 'delivery') {
+      zone2Deliveries = postData.zone === '2' ? 1 : 0;
+      zone3Deliveries = postData.zone === '3' ? 1 : 0;
+    } else if (postData['delivery-type'] === 'split-delivery') {
+      zone2Deliveries = Object.entries(postData).filter((x) => x[0].startsWith('zone-') && x[1] === '2').length;
+      zone3Deliveries = Object.entries(postData).filter((x) => x[0].startsWith('zone-') && x[1] === '3').length;
+    }
+    const delivery = JSON.stringify({
+      zone2: zone2Deliveries,
+      zone3: zone3Deliveries
+    });
+
+    const axiosConfig = {
+      method: 'post',
+      url: `${managementBaseUrl}/treatbox/lookupprice`,
+      data: {
+        basket,
+        delivery,
+        codes: postData['rebate-codes']
+      }
+    };
+    try {
+      const response = await axios(axiosConfig);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  // Function to generate a dataLayer object for GTM tracking
   function generateDataLayer(statement, event) {
     const { products } = statement;
     const dataLayer = {
+      event,
       ecommerce: {
         items: []
       }
@@ -26,14 +74,6 @@ function whiskController() {
         quantity: product.quantity
       });
     });
-    switch (event) {
-      case 'checkout':
-        dataLayer.event = 'begin_checkout';
-        break;
-
-      default:
-        break;
-    }
     return JSON.stringify(dataLayer);
   }
 
@@ -68,52 +108,11 @@ function whiskController() {
     const orderPlacedUrl = new URL(`${req.protocol}://${req.get('host')}/orderplaced`);
     const formSubmitUrl = new URL(`${managementBaseUrl}/treatbox/payment`);
 
-    // Get Price Information
-    let basket = [];
-    const orders = Object.entries(req.body).filter((x) => x[0].startsWith('quantity-'));
-    for (let i = 0; i < orders.length; i += 1) {
-      const [key, q] = orders[i];
-      const [, id] = key.split('-');
-      const quantity = parseInt(q, 10);
-      if (quantity > 0) {
-        basket.push({ id, quantity });
-      }
-    }
-    basket = JSON.stringify(basket);
+    // Get Statement
+    const statement = await getStatement(req.body);
 
-    // Get Delivery Information
-    let zone2Deliveries;
-    let zone3Deliveries;
-    if (req.body['delivery-type'] === 'delivery') {
-      zone2Deliveries = req.body.zone === '2' ? 1 : 0;
-      zone3Deliveries = req.body.zone === '3' ? 1 : 0;
-    } else if (req.body['delivery-type'] === 'split-delivery') {
-      zone2Deliveries = Object.entries(req.body).filter((x) => x[0].startsWith('zone-') && x[1] === '2').length;
-      zone3Deliveries = Object.entries(req.body).filter((x) => x[0].startsWith('zone-') && x[1] === '3').length;
-    }
-    const delivery = JSON.stringify({
-      zone2: zone2Deliveries,
-      zone3: zone3Deliveries
-    });
-
-    const axiosConfig = {
-      method: 'post',
-      url: `${managementBaseUrl}/treatbox/lookupprice`,
-      data: {
-        basket,
-        delivery,
-        codes: req.body['rebate-codes']
-      }
-    };
-    let priceInformation;
-    try {
-      const response = await axios(axiosConfig);
-      priceInformation = response.data;
-    } catch (error) {
-      debug(error);
-    }
-
-    const googleDataLayer = generateDataLayer(priceInformation, 'checkout');
+    // Get Data Layer for GTM Tracking
+    const googleDataLayer = generateDataLayer(statement, 'begin_checkout');
 
     const payload = {};
     Object.entries(req.body).forEach((item) => {
@@ -130,7 +129,7 @@ function whiskController() {
       formSubmitUrl,
       body: req.body,
       query: req.query,
-      priceInformation,
+      statement,
       priceFormat,
       dateFormat,
       parseDateCode,
@@ -140,10 +139,15 @@ function whiskController() {
   }
 
   async function orderPlaced(req, res) {
+    // Get Data Layer for GTM Tracking
+    const statement = await getStatement(req.body);
+    const googleDataLayer = generateDataLayer(statement, 'purchase');
+
     return res.render('orderplaced', {
       googleApiKey,
       page: 'orderplaced',
-      body: req.body
+      body: req.body,
+      googleDataLayer
     });
   }
 

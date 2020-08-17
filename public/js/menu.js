405b8d45617e0a0ccaae7ba8e19f4ffcab507c34
Stephen Bunting <stebunting@gmail.com>
2020-08-17 12:11:04 +0200

    Remove JQuery from menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,8 +1,8 @@
-$(() => {
-  // Display cake cutting diagrams
-  $('.view-cake-image').click(function callback() {
-    const img = $(this).attr('id');
-    const imgHtml = `<img class="cake-guide" src="/images/cake-cutting-guide-${img}.jpg" alt="Cake Cutting Guide" />`;
-    $('#cakeCuttingImg').html(imgHtml);
+// Display cake cutting diagrams
+const links = document.getElementsByClassName('view-cake-image');
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener('click', () => {
+    document.getElementById('cake-cutting-image')
+      .innerHTML = `<img class="cake-guide" src="/images/cake-cutting-guide-${links[i].id}.jpg" alt="Cake Cutting Guide" />`;
   });
-});
+}
